52997aefea612f285defdd3c5e5f03ac644b7fef
Fred Myerscough <oniice@gmail.com>
2022-01-24 17:14:53 +0000

    refactor: updated project for php 8.1 baseline

--- src/Utility.php
+++ src/Utility.php
@@ -6,49 +6,54 @@ use DivisionByZeroError;
 use Myerscode\Utilities\Numbers\Exceptions\InvalidNumberException;
 use Myerscode\Utilities\Numbers\Exceptions\IsZeroException;
 use Myerscode\Utilities\Numbers\Exceptions\NonNumericValueException;
+use Stringable;
 
 /**
  * Class Utility
  *
  * @package Myerscode\Utilities\Numbers
  */
-class Utility
+class Utility implements Stringable
 {
     /**
      * The value this model is representing
      *
-     * @var number
+     * @var  mixed  number
      */
-    private $number;
+    private readonly mixed $number;
 
     /**
      * Utility constructor.
      *
-     * @param $number
-     *
      * @throws NonNumericValueException
      */
-    public function __construct($number)
+    public function __construct(int|float|string|Utility $number)
     {
         if (is_numeric($number)) {
             // set the value to a numeric format
             $number += 0;
+        } elseif (empty($number)) {
+            $number = 0;
         } else {
-            if (empty($number)) {
-                $number = 0;
-            } else {
-                $message = gettype($number) . ' cannot be cast to a valid number';
-                throw new NonNumericValueException($message);
-            }
+            $message = gettype($number).' cannot be cast to a valid number';
+            throw new NonNumericValueException($message);
         }
 
         $this->number = $number;
     }
 
     /**
-     * Return the value when casting to string
+     * Create a new instance of the number utility
      *
-     * @return string
+     * @throws NonNumericValueException
+     */
+    public static function make(int|float|string|Utility $number): Utility
+    {
+        return new Utility($number);
+    }
+
+    /**
+     * Return the value when casting to string
      */
     public function __toString(): string
     {
@@ -58,58 +63,50 @@ class Utility
     /**
      * Add a number to current number
      *
-     * @param $number
-     *
-     * @return Utility
      * @throws NonNumericValueException
      */
-    public function add($number): Utility
+    public function add(int|float|string|Utility $number): Utility
     {
         $number = $this->number + (new static($number))->value();
 
-        return new static($number);
+        return static::make($number);
     }
 
     /**
      * Ceil the number
      *
-     * @return Utility
      * @throws NonNumericValueException
      */
     public function ceil(): Utility
     {
         $value = ceil($this->number);
 
-        return new static($value);
+        return static::make($value);
     }
 
     /**
      * Divide the number by the number
      *
-     * @param $number
-     *
-     * @return Utility
      * @throws NonNumericValueException
-     * @throws \DivisionByZeroError
      */
-    public function divide($number): Utility
+    public function divide(int|float|string|Utility $number): Utility
     {
-        $divisible = new static($number);
+        $static = static::make($number);
 
-        if ($divisible->value() == 0 || $this->number == 0) {
+        if ($static->value() == 0 || $this->number == 0) {
             throw new DivisionByZeroError();
         }
 
-        $value = $this->number / $divisible->value();
+        $value = $this->number / $static->value();
 
-        return new static($value);
+        return static::make($value);
     }
 
     /**
      * Get the factors for the number
      *
-     * @return array
      * @throws InvalidNumberException
+     * @return float[]|int[]
      */
     public function factors(): array
     {
@@ -122,7 +119,7 @@ class Utility
         $sqrx = floor(sqrt($x));
 
         $factors = [];
-        for ($i = 1; $i <= $sqrx; $i++) {
+        for ($i = 1; $i <= $sqrx; ++$i) {
             if ($x % $i === 0) {
                 $factors[] = $i;
                 if ($i !== $sqrx) {
@@ -130,200 +127,118 @@ class Utility
                 }
             }
         }
+
         sort($factors);
+
         return $factors;
     }
 
     /**
      * Floor the number
      *
-     * @return Utility
      * @throws NonNumericValueException
      */
     public function floor(): Utility
     {
         $value = floor($this->number);
 
-        return new static($value);
+        return static::make($value);
     }
 
     /**
      * Is the number negative
      *
-     * @return bool
      * @throws IsZeroException
      */
     public function isNegative(): bool
     {
-        if ($this->number === 0 ){
+        if ($this->number === 0) {
             throw new IsZeroException('0 is neither positive or negative');
         }
-        return (!is_int($this->number) && $this->number < 0 || $this->number < 0);
-    }
 
-    /**
-     * Create a new instance of the number utility
-     *
-     * @param $number
-     *
-     * @return Utility
-     * @throws NonNumericValueException
-     */
-    public static function make($number): Utility
-    {
-        return new static($number);
+        return (!is_int($this->number) && $this->number < 0 || $this->number < 0);
     }
 
     /**
      * Get the order of magnitude of the number
      *
-     * @return Utility
      * @throws NonNumericValueException
      */
     public function magnitude(): Utility
     {
-        if ($this->number == 0) {
-            $magnitude = 0;
-        } else {
-            $magnitude = floor(log10(abs($this->number)));
-        }
+        $magnitude = $this->number == 0 ? 0 : floor(log10(abs($this->number)));
 
-        return new static($magnitude);
+        return static::make($magnitude);
     }
 
     /**
      * Minus a number
      *
-     * @param $number
-     *
-     * @return Utility
      * @throws NonNumericValueException
      */
-    public function minus($number): Utility
+    public function minus(int|float|string|Utility $number): Utility
     {
-        $value = $this->number - (new static($number))->value();
+        $value = $this->number - (static::make($number))->value();
 
-        return new static($value);
+        return static::make($value);
     }
 
     /**
      * Multiply a number
      *
-     * @param $number
-     *
-     * @return Utility
      * @throws NonNumericValueException
      */
-    public function multiply($number): Utility
+    public function multiply(int|float|string|Utility $number): Utility
     {
-        $value = $this->number * (new static($number))->value();
+        $value = $this->number * (static::make($number))->value();
 
-        return new static($value);
+        return static::make($value);
     }
 
     /**
      * Returns the ordinal version of a number (appends th, st, nd, rd).
-     *
-     * @return string
      */
     public function ordinal(): string
     {
         $abs = abs($this->number);
 
-        switch ($abs % 10) {
-            case 1:
-                $ordinal = ($abs % 100 === 11) ? 'th' : 'st';
-                break;
-            case 2:
-                $ordinal = ($abs % 100 === 12) ? 'th' : 'nd';
-                break;
-            case 3:
-                $ordinal = ($abs % 100 === 13) ? 'th' : 'rd';
-                break;
-            default:
-                $ordinal = 'th';
-                break;
-        }
-
-        return $ordinal;
+        return match ($abs % 10) {
+            1 => ($abs % 100 === 11) ? 'th' : 'st',
+            2 => ($abs % 100 === 12) ? 'th' : 'nd',
+            3 => ($abs % 100 === 13) ? 'th' : 'rd',
+            default => 'th',
+        };
     }
 
     /**
-     * Add padding to the number
-     *
-     * @param int $padding
-     * @param int $direction
-     *
-     * @return string
+     * Add padding on the right of the number, with a given padding value
      */
-    private function pad($padding = 1, $direction = STR_PAD_BOTH): string
-    {
-        return str_pad($this->number, $padding, 0, $direction);
-    }
-
-    /**
-     * Add padding on the left of an the number.
-     *
-     * @param int $padding
-     *
-     * @return string
-     */
-    public function padLeft($padding = 1): string
+    public function padLeft(int $padding = 1): string
     {
         return $this->pad($padding, STR_PAD_LEFT);
     }
 
     /**
-     * Add padding on the right of an the number.
-     *
-     * @param int $padding
-     *
-     * @return string
+     * Add padding on the right of the number, with a given padding value
      */
-    public function padRight($padding = 1): string
+    public function padRight(int $padding = 1): string
     {
         return $this->pad($padding, STR_PAD_RIGHT);
     }
 
     /**
-     * @param int $exponent
+     * Get the number to a given power
      *
-     * @return Utility
      * @throws NonNumericValueException
      */
     public function power(int $exponent): Utility
     {
-        return new static(pow($this->number, $exponent));
-    }
-
-    /**
-     * Round a number
-     *
-     * @param $number
-     * @param int $precision
-     * @param int $mode
-     *
-     * @return Utility
-     * @throws InvalidNumberException
-     * @throws NonNumericValueException
-     */
-    private function round($number, $precision, int $mode): Utility
-    {
-        if ($precision < 0) {
-            throw new InvalidNumberException('Precision value should be greater or equal to zero');
-        }
-
-        $value = round($number, $precision, $mode);
-
-        return new static($value);
+        return static::make($this->number ** $exponent);
     }
 
     /**
      * Round down the number
      *
-     * @param int $precision
-     *
-     * @return Utility
      * @throws InvalidNumberException
      * @throws NonNumericValueException
      */
@@ -335,9 +250,6 @@ class Utility
     /**
      * Round up the number
      *
-     * @param int $precision
-     *
-     * @return Utility
      * @throws InvalidNumberException
      * @throws NonNumericValueException
      */
@@ -348,8 +260,6 @@ class Utility
 
     /**
      * The numbers type
-     *
-     * @return string
      */
     public function type(): string
     {
@@ -365,22 +275,43 @@ class Utility
     /**
      * Get the current value of the number
      *
-     * @return number
+     * @return int|float
      */
-    public function value()
+    public function value(): mixed
     {
         return $this->number;
     }
 
     /**
      * Returns the ordinal version of a number (appends th, st, nd, rd).
-     *
-     * @param string $spacer
-     *
-     * @return string
      */
-    public function withOrdinal($spacer = ''): string
+    public function withOrdinal(string $spacer = ''): string
     {
-        return $this->number . $spacer . $this->ordinal();
+        return $this->number.$spacer.$this->ordinal();
+    }
+
+    /**
+     * Add padding to the number
+     */
+    private function pad(int $padding = 1, int $direction = STR_PAD_BOTH): string
+    {
+        return str_pad($this->number, $padding, 0, $direction);
+    }
+
+    /**
+     * Round a number
+     *
+     * @throws InvalidNumberException
+     * @throws NonNumericValueException
+     */
+    private function round(float|int $number, int $precision, int $mode): Utility
+    {
+        if ($precision < 0) {
+            throw new InvalidNumberException('Precision value should be greater or equal to zero');
+        }
+
+        $value = round($number, $precision, $mode);
+
+        return static::make($value);
     }
 }
